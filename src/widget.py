12c74eefe8cb129b4f309addcec62b8919ae213c
Арина Новгородова <arrrishanov@gmail.com>
2025-06-09 13:38:41 +0300

    add update mask_account_card function

---
+++ src/widget.py
@@ -0,0 +1,14 @@
+from typing import Union
+from src.masks import get_mask_account
+from src.masks import get_mask_card_number
+
+def mask_account_card(card_accaunt_number: (Union[str])) -> Union[str]:
+    if "Счет" in card_accaunt_number:
+        account_number = card_accaunt_number[5:]
+        mask_accunt = get_mask_account(account_number)
+        return f"{card_accaunt_number[:4]} {mask_accunt}"
+    else:
+        card_number = card_accaunt_number[-16:]
+        card_name = card_accaunt_number[:-17]
+        mask_card = get_mask_card_number(card_number)
+        return f"{card_name} {mask_card}"
