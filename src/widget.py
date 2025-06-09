f135838d7796749f84d9ecf1ddd59edd1968b85f
Арина Новгородова <arrrishanov@gmail.com>
2025-06-09 14:07:18 +0300

    small fixes

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,9 @@
 from typing import Union
-from src.masks import get_mask_account
-from src.masks import get_mask_card_number
 
-def mask_account_card(card_accaunt_number: (Union[str])) -> Union[str]:
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card_accaunt_number: Union[str]) -> Union[str]:
     if "Счет" in card_accaunt_number:
         account_number = card_accaunt_number[5:]
         mask_accunt = get_mask_account(account_number)
@@ -14,8 +15,7 @@ def mask_account_card(card_accaunt_number: (Union[str])) -> Union[str]:
         return f"{card_name} {mask_card}"
 
 
-
-def get_date(data: Union[str]) -> Union[str]:
+def (data: Union[str]) -> Union[str]:
     """Функция, которая преобразует дату"""
     day = data[8:10]
     month = data[5:7]
