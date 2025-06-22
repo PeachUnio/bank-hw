8c50d0add7e04cf88e703a81e3ad497a5309d445
Арина Новгородова <arrrishanov@gmail.com>
2025-06-22 21:24:54 +0300

    correction of grammatical errors

--- src/widget.py
+++ src/widget.py
@@ -3,15 +3,15 @@ from typing import Union
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(card_accaunt_number: Union[str]) -> Union[str]:
+def mask_account_card(card_account_number: Union[str]) -> Union[str]:
     """Функция, которая маскирует имя аккаунта или счета"""
-    if "Счет" in card_accaunt_number:
-        account_number = card_accaunt_number[5:]
-        mask_accunt = get_mask_account(account_number)
-        return f"{card_accaunt_number[:4]} {mask_accunt}"
+    if "Счет" in card_account_number:
+        account_number = card_account_number[5:]
+        mask_account = get_mask_account(account_number)
+        return f"{card_account_number[:4]} {mask_account}"
     else:
-        card_number = card_accaunt_number[-16:]
-        card_name = card_accaunt_number[:-17]
+        card_number = card_account_number[-16:]
+        card_name = card_account_number[:-17]
         mask_card = get_mask_card_number(card_number)
         return f"{card_name} {mask_card}"
 
