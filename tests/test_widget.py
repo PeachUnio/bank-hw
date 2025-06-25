9be40e0c2f4d6eea9c344dcab5888539ef1e21ea
Арина Новгородова <arrrishanov@gmail.com>
2025-06-25 20:32:52 +0300

    fix tests for widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -12,3 +12,4 @@ def test_get_date():
     assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
 
     assert get_date("2000-11-06T02:26:18.671407") == "06.11.2000"
+    assert get_date("") == ""
