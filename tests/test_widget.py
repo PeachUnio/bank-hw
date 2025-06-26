874d9562ea50afa326d279dbfc5c51969016235d
Арина Новгородова <arrrishanov@gmail.com>
2025-06-26 20:52:43 +0300

    add parametrize for test_widget

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,12 +1,14 @@
+import pytest
+
 from src.widget import mask_account_card, get_date
 
+@pytest.mark.parametrize("card_account, mask_number", [("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+                                                       ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+                                                       ("Счет 73654108430135874305", "Счет **4305"),
+                                                       ("Счет 35383033474447895560", "Счет **5560")])
 
-def test_mask_account_card():
-    assert mask_account_card("Maestro 1596837868705199") == "Maestro 1596 83** **** 5199"
-    assert mask_account_card("Visa Platinum 8990922113665229") == "Visa Platinum 8990 92** **** 5229"
-
-    assert mask_account_card("Счет 73654108430135874305") == "Счет **4305"
-    assert mask_account_card("Счет 35383033474447895560") == "Счет **5560"
+def test_mask_account_card(card_account, mask_number):
+    assert mask_account_card(card_account) == mask_number
 
 def test_get_date():
     assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
