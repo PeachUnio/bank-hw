c84cefe7ce7fdb102d99a3acd432daf73f056398
Арина Новгородова <arrrishanov@gmail.com>
2025-06-26 11:02:04 +0300

    add parametrize for test_masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,16 +1,19 @@
+import pytest
+
 from src.masks import get_mask_card_number, get_mask_account
 
-def test_get_mask_card_number():
-    assert get_mask_card_number(7000792289606361) == "7000 79** **** 6361"
 
-    assert get_mask_card_number(7779077786666688886) == "7779 07** **** 6688886"
+@pytest.mark.parametrize("card, masks_number", [(7000792289606361, "7000 79** **** 6361"),
+                                                (7779077786666688886, "7779 07** **** 6688886"),
+                                                ("", "")])
 
-    assert get_mask_card_number("") == ""
+def test_get_mask_card_number(card, masks_number):
+    assert get_mask_card_number(card) == masks_number
 
 
-def test_get_mask_account():
-    assert get_mask_account(73654108430135874305) == "**4305"
+@pytest.mark.parametrize("account, masks_account", [(73654108430135874305, "**4305"),
+                                                    (48387853955938559393585853, "**5853"),
+                                                    (665798797, "**8797")])
 
-    assert get_mask_account(48387853955938559393585853) == "**5853"
-
-    assert get_mask_account(665798797) == "**8797"
+def test_get_mask_account(account, masks_account):
+    assert get_mask_account(account) == masks_account
