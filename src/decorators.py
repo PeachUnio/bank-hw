a20a3579e810b51e0d863b22cc628da6c116f223
Арина Новгородова <arrrishanov@gmail.com>
2025-07-14 03:03:26 +0300

    sintaksis fixses

--- src/decorators.py
+++ src/decorators.py
@@ -1,10 +1,9 @@
-import time
+# import time
 from functools import wraps
 
 
-
 def format_inputs(args, kwargs):
-    """"Функция, которая формирует аргументы"""
+    """ "Функция, которая формирует аргументы"""
     parts = []
     if args:
         parts.append(f"args: {args}")
@@ -13,15 +12,15 @@ def format_inputs(args, kwargs):
     return ", ".join(parts) if parts else "no inputs"
 
 
-#декоратор фиксирующий время выполнения функции, ее результат и возможные ошибки
+# декоратор фиксирующий время выполнения функции, ее результат и возможные ошибки
 def log(filename=None):
     def wrapper(func):
         @wraps(func)
         def inner(*args, **kwargs):
-            start_time = time.perf_counter()
+            # start_time = time.perf_counter()
             try:
                 result = func(*args, **kwargs)
-                end_time = time.perf_counter()
+                # end_time = time.perf_counter()
                 message = f"{result}\n{func.__name__} ok"
                 if filename is None:
                     print(message)
@@ -31,7 +30,7 @@ def log(filename=None):
                     print(f"{func.__name__} ok")
                 return result
             except Exception as e:
-                end_time = time.perf_counter()
+                # end_time = time.perf_counter()
                 inputs = format_inputs(args, kwargs)
                 message = f"{func.__name__} error: {e} Inputs: {inputs}"
                 if filename is None:
@@ -40,5 +39,7 @@ def log(filename=None):
                     with open(filename, "a", encoding="utf-8") as file:
                         file.write(message)
                 raise
+
         return inner
+
     return wrapper
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -2,7 +2,8 @@ from os import access
 
 import pytest
 
-from src.decorators import log, format_inputs
+from src.decorators import format_inputs, log
+
 
 def test_log_print(capsys):
     @log()
@@ -54,9 +55,10 @@ def test_log_file_error(tmp_path):
         content = file.read()
         assert "divide_numbers error: division by zero Inputs: args: (13, 0)" in content
 
+
 def test_format_inputs():
     assert "args: (1, 2)" in format_inputs((1, 2), {})
 
-    assert "kwargs: {'a': 1}" in format_inputs((), {'a': 1})
+    assert "kwargs: {'a': 1}" in format_inputs((), {"a": 1})
 
     assert "no inputs" == format_inputs((), {})
