e78bddc258949a8e20978d7ebbffcf9441c0620a
Арина Новгородова <arrrishanov@gmail.com>
2025-07-18 19:32:50 +0300

    add load_transactions function

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import json
+from json import JSONDecodeError
+import requests
+
+
+def load_transactions(file_path):
+    """Функция принимает путь до JSON-файла и возвращает список транзакций"""
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            json_data = json.load(file)
+        if isinstance(json_data, list):
+            return json_data
+        else:
+            return []
+    except JSONDecodeError:
+        return []
+    except TypeError:
+        return []
+    except FileNotFoundError:
+        return []
