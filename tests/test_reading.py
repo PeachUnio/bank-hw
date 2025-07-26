3fc8da33a579bc853707a1d9f1d862d4f7fe042a
Арина Новгородова <arrrishanov@gmail.com>
2025-07-27 01:56:00 +0300

    add test for reading_excel_file functions

--- tests/test_reading.py
+++ tests/test_reading.py
@@ -24,3 +24,23 @@ def test_reading_csv_file_error(mock_read_csv):
     mock_read_csv.side_effect = Exception("NotFound")
     result = reading_csv_file("error.csv")
     assert result == "Поизошла ошибка NotFound"
+
+
+@patch("pandas.read_excel")
+def test_reading_excel_file(mock_read_excel):
+    test_data = pd.DataFrame({"id":[650703, 3107343], "date": ["EXECUTED", "CANCELED"]})
+    mock_read_excel.return_value = test_data
+
+    result = reading_excel_file("succes.csv")
+    assert result == [{"id": 650703, "date": "EXECUTED"}, {"id": 3107343, "date": "CANCELED"}]
+    mock_read_excel.assert_called_once_with("succes.csv")
+
+def test_reading_excel_file_empty_path():
+    result = reading_excel_file("")
+    assert result == "Путь не найден"
+
+@patch("pandas.read_excel")
+def test_reading_excel_file_error(mock_read_excel):
+    mock_read_excel.side_effect = Exception("NotFound")
+    result = reading_excel_file("error.csv")
+    assert result == "Поизошла ошибка NotFound"
