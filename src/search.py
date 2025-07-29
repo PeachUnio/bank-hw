db08834f08752a1cdb664f1f21aa6b33ebe52232
Арина Новгородова <arrrishanov@gmail.com>
2025-07-29 20:10:40 +0300

    add process_bank_operations function

--- src/search.py
+++ src/search.py
@@ -1,9 +1,26 @@
 import re
+from collections import defaultdict
+from collections import Counter
 
 
 def process_bank_search(data, search):
     """
-    Фильтрует операции по строке поиска в описании (с использованием regex)
+    Фильтрует операции по строке поиска в описании
     """
     pattern = re.compile(re.escape(search), re.IGNORECASE)
-    return [op for op in data if 'description' in op and pattern.search(op['description'])]
+    return [op for op in data if "description" in op and pattern.search(op["description"])]
+
+
+def process_bank_operations(data, categories):
+    """Функция, которая подсчитывает кол-во операций в каждой категории"""
+    new_dict = []
+    patterns = {category: re.compile(re.escape(category), re.IGNORECASE)
+                for category in categories}
+    for op in data:
+        if "description" not in op:
+            continue
+        description = op["description"]
+        for category, pattern in patterns.items():
+            if pattern.search(description):
+                new_dict.append(category)
+    return dict(Counter(new_dict))
