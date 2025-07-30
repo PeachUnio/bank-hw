a6b936eabfd4e1d45e49d769277ff4b5825b249a
Арина Новгородова <arrrishanov@gmail.com>
2025-07-30 09:40:04 +0300

    small fixes

--- src/masks.py
+++ src/masks.py
@@ -4,9 +4,7 @@ from typing import Union
 logger = logging.getLogger("masks")
 logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler("logs/masks.log")
-file_formatter = logging.Formatter(
-    "%(asctime)s - %(filename)s - %(levelname)s: %(message)s"
-)
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
@@ -16,14 +14,7 @@ def get_mask_card_number(card_number: Union[int]) -> Union[str]:
     if card_number:
         logger.info(f"Маскирует номер карты {card_number}")
         str_card_number = str(card_number)
-        masking_card_number = (
-            str_card_number[:4]
-            + " "
-            + str_card_number[4:6]
-            + "** ****"
-            + " "
-            + str_card_number[12:]
-        )
+        masking_card_number = str_card_number[:4] + " " + str_card_number[4:6] + "** ****" + " " + str_card_number[12:]
         return masking_card_number
     logger.error("Не было передано номера карты")
     return ""
